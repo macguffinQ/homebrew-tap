414633c9cc4c00ef85f7752a87f69b64988f0823
allendred <allendred@163.com>
2026-06-22 13:19:03 +0800

    Update tiffany-loop to v0.1.23

--- Formula/tiffany-loop.rb
+++ Formula/tiffany-loop.rb
@@ -3,11 +3,11 @@ class TiffanyLoop < Formula
   homepage "https://github.com/macguffinQ/Tiffany"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/macguffinQ/Tiffany/releases/download/v0.1.22/tiffany-loop-v0.1.22-aarch64-apple-darwin.tar.gz"
-    sha256 "9fdcdf23a7366f931382412ccb083e552c04f0fb9a4e8d99e9ab7653c5a5844a"
+    url "https://github.com/macguffinQ/Tiffany/releases/download/v0.1.23/tiffany-loop-v0.1.23-aarch64-apple-darwin.tar.gz"
+    sha256 "462c89d72d663df354d95063b38a2c47361daee871abe11965aa9391fb860d25"
   else
-    url "https://github.com/macguffinQ/Tiffany/archive/refs/tags/v0.1.22.tar.gz"
-    sha256 "7e381863758d64ada5bede75128fa1e809c1f5274d47aa61f9490388c5d0c3c1"
+    url "https://github.com/macguffinQ/Tiffany/archive/refs/tags/v0.1.23.tar.gz"
+    sha256 "56c72a843fd66a2b6b9d679f6648de432a6da7580ed0a2d7eabe454d4c112000"
     depends_on "rust" => :build
   end
 
